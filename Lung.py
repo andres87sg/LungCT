5c484c880cb35b32a119c49830944d76f85fab01
asandino87 <asandino@unal.edu.co>
2021-04-06 15:49:55 -0500

    Update Lung.py

--- Lung.py
+++ Lung.py
@@ -16,20 +16,35 @@ import os
 
 #%% Main program
 
-origpath = 'C:/Users/Andres/Desktop/dicomimage/Patient3/' 
+origpath = 'C:/Users/Andres/Desktop/dicomimage/Patient1/' 
 listfiles = os.listdir(origpath)
-destpath = 'C:/Users/Andres/Desktop/imexhs/Lung/converted3/'
+destpath = 'C:/Users/Andres/Desktop/imexhs/Lung/converted/'
+patient = 'patient1'
 
 for i in range(len(listfiles)):
 #for i in [1]:
 #    print(listfiles[i])
     dcmfilename = listfiles[i]
-    norm_img = dcm_convert(origpath,dcmfilename)    
+    [norm_img, ins_num] = dcm_convert(origpath,dcmfilename)    
+    print(ins_num)
+    
+    if np.int8(ins_num)<10:
+        ins_num='0'+str(ins_num)
+
+    if np.int8(ins_num)<100:
+        ins_num='0'+str(ins_num)
+        
+    if np.int8(ins_num)>=100:
+        ins_num=str(ins_num)
+        
+    print(ins_num)
+    
     #plt.figure()
     #plt.imshow(norm_img,cmap='gray')
     #plt.axis('off')
     imgformat = '.png'
-    image_dest = destpath + dcmfilename + imgformat
+    #image_dest = destpath + ins_num +dcmfilename + imgformat
+    image_dest = destpath + ins_num + '_' + patient  + imgformat
     cv2.imwrite(image_dest, norm_img)
     
     
@@ -43,6 +58,7 @@ def dcm_convert(dcm_dir,dcmfilename):
     
     
     dcm_img = dicom.dcmread(img_path)
+    instance_number=dcm_img.InstanceNumber
     
     # Convert dicom image to pixel array
     img_array = dcm_img.pixel_array
@@ -62,7 +78,7 @@ def dcm_convert(dcm_dir,dcmfilename):
     norm_img=cv2.normalize(lungwin_img, None, alpha = 0, beta = 255, norm_type = cv2.NORM_MINMAX, dtype = cv2.CV_32F)
     norm_img=np.uint8(norm_img)
     
-    return norm_img
+    return norm_img, instance_number
     
 #dcm_image.WindowCenter
 #dcm_image.WindowWidth
