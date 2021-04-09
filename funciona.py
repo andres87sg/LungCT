855fcfad76921008d684ff4c33d5687bf7a648a7
asandino87 <asandino@unal.edu.co>
2021-04-08 21:18:36 -0500

    Nueva Version
    Agregando el clasificador U-Net

---
+++ funciona.py
@@ -0,0 +1,188 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr  8 19:18:03 2021
+
+@author: Andres
+"""
+
+import os
+
+input_dir = "C:/Users/Andres/Desktop/imexhs/Lung/Prueba/CT/F1/"
+target_dir = "C:/Users/Andres/Desktop/imexhs/Lung/Prueba/mask/F1"
+img_size = (512, 512)
+num_classes = 1
+batch_size = 2
+
+input_img_paths = sorted(
+    [
+        os.path.join(input_dir, fname)
+        for fname in os.listdir(input_dir)
+        if fname.endswith(".png")
+    ]
+)
+target_img_paths = sorted(
+    [
+        os.path.join(target_dir, fname)
+        for fname in os.listdir(target_dir)
+        if fname.endswith(".png") and not fname.startswith(".")
+    ]
+)
+
+print("Number of samples:", len(input_img_paths))
+
+for input_path, target_path in zip(input_img_paths[:2], target_img_paths[:2]):
+    print(input_path, "|", target_path)
+
+#%%
+
+from IPython.display import Image, display
+from tensorflow.keras.preprocessing.image import load_img
+import PIL
+from PIL import ImageOps
+
+# Display input image #7
+display(Image(filename=input_img_paths[1]))
+
+# Display auto-contrast version of corresponding target (per-pixel categories)
+img = PIL.ImageOps.autocontrast(load_img(target_img_paths[1]))
+display(img)
+
+#%%
+
+from tensorflow import keras
+import numpy as np
+from tensorflow.keras.preprocessing.image import load_img
+
+
+class OxfordPets(keras.utils.Sequence):
+    """Helper to iterate over the data (as Numpy arrays)."""
+
+    def __init__(self, batch_size, img_size, input_img_paths, target_img_paths):
+        self.batch_size = batch_size
+        self.img_size = img_size
+        self.input_img_paths = input_img_paths
+        self.target_img_paths = target_img_paths
+
+    def __len__(self):
+        return len(self.target_img_paths) // self.batch_size
+
+    def __getitem__(self, idx):
+        """Returns tuple (input, target) correspond to batch #idx."""
+        i = idx * self.batch_size
+        batch_input_img_paths = self.input_img_paths[i : i + self.batch_size]
+        batch_target_img_paths = self.target_img_paths[i : i + self.batch_size]
+        x = np.zeros((self.batch_size,) + self.img_size + (3,), dtype="float32")
+        for j, path in enumerate(batch_input_img_paths):
+            img = load_img(path, target_size=self.img_size)
+            x[j] = img
+        y = np.zeros((self.batch_size,) + self.img_size + (1,), dtype="uint8")
+        for j, path in enumerate(batch_target_img_paths):
+            img = load_img(path, target_size=self.img_size, color_mode="grayscale")
+            y[j] = np.expand_dims(img, 2)
+            # Ground truth labels are 1, 2, 3. Subtract one to make them 0, 1, 2:
+            y[j] -= 1
+        return x, y
+    
+#%%
+
+from tensorflow.keras import layers
+
+
+def get_model(img_size, num_classes):
+    inputs = keras.Input(shape=img_size + (3,))
+
+    ### [First half of the network: downsampling inputs] ###
+
+    # Entry block
+    x = layers.Conv2D(32, 3, strides=2, padding="same")(inputs)
+    x = layers.BatchNormalization()(x)
+    x = layers.Activation("relu")(x)
+
+    previous_block_activation = x  # Set aside residual
+
+    # Blocks 1, 2, 3 are identical apart from the feature depth.
+    for filters in [64, 128, 256]:
+        x = layers.Activation("relu")(x)
+        x = layers.SeparableConv2D(filters, 3, padding="same")(x)
+        x = layers.BatchNormalization()(x)
+
+        x = layers.Activation("relu")(x)
+        x = layers.SeparableConv2D(filters, 3, padding="same")(x)
+        x = layers.BatchNormalization()(x)
+
+        x = layers.MaxPooling2D(3, strides=2, padding="same")(x)
+
+        # Project residual
+        residual = layers.Conv2D(filters, 1, strides=2, padding="same")(
+            previous_block_activation
+        )
+        x = layers.add([x, residual])  # Add back residual
+        previous_block_activation = x  # Set aside next residual
+
+    ### [Second half of the network: upsampling inputs] ###
+
+    for filters in [256, 128, 64, 32]:
+        x = layers.Activation("relu")(x)
+        x = layers.Conv2DTranspose(filters, 3, padding="same")(x)
+        x = layers.BatchNormalization()(x)
+
+        x = layers.Activation("relu")(x)
+        x = layers.Conv2DTranspose(filters, 3, padding="same")(x)
+        x = layers.BatchNormalization()(x)
+
+        x = layers.UpSampling2D(2)(x)
+
+        # Project residual
+        residual = layers.UpSampling2D(2)(previous_block_activation)
+        residual = layers.Conv2D(filters, 1, padding="same")(residual)
+        x = layers.add([x, residual])  # Add back residual
+        previous_block_activation = x  # Set aside next residual
+
+    # Add a per-pixel classification layer
+    outputs = layers.Conv2D(num_classes, 3, activation="softmax", padding="same")(x)
+
+    # Define the model
+    model = keras.Model(inputs, outputs)
+    return model
+
+
+# Free up RAM in case the model definition cells were run multiple times
+keras.backend.clear_session()
+
+# Build model
+model = get_model(img_size, num_classes)
+model.summary()
+
+#%%
+
+import random
+
+# Split our img paths into a training and a validation set
+val_samples = 5
+#random.Random(2).shuffle(input_img_paths)
+#random.Random(2).shuffle(target_img_paths)
+train_input_img_paths = input_img_paths[:val_samples]
+train_target_img_paths = target_img_paths[:val_samples]
+val_input_img_paths = input_img_paths[val_samples:]
+val_target_img_paths = target_img_paths[val_samples:]
+
+# Instantiate data Sequences for each split
+train_gen = OxfordPets(
+    batch_size, img_size, train_input_img_paths, train_target_img_paths
+)
+val_gen = OxfordPets(batch_size, img_size, val_input_img_paths, val_target_img_paths)
+
+#%%
+
+# Configure the model for training.
+# We use the "sparse" version of categorical_crossentropy
+# because our target data is integers.
+model.compile(optimizer="rmsprop", loss="categorical_crossentropy")
+
+callbacks = [
+    keras.callbacks.ModelCheckpoint("oxford_segmentation.h5", save_best_only=True)
+]
+
+# Train the model, doing validation at the end of each epoch.
+epochs = 5
+model.fit(train_gen, epochs=epochs, validation_data=val_gen, callbacks=callbacks)
