f62944ed1c758898d4757aebad48415db7f88fe6
asandino87 <asandino@unal.edu.co>
2021-05-04 15:08:06 -0500

    up

--- ConvertImages/get_nii2png.py
+++ ConvertImages/get_nii2png.py
@@ -3,6 +3,8 @@ Created on Apr 14 2021
 Modified on May 05 2021
 
 @author: Andres Sandino
+
+Convert "nii" image format in "png" in Lung WW=-500,WL=1500
 """
 
 import os
@@ -15,21 +17,64 @@ import nibabel as nib
 #%%
 
 # Patient number
-patient_no = 25
+patient_no = 1
 
-# Origin path
-path = 'C:/Users/Andres/Downloads/Estudio15.nii'
+# Origin path and filename
+path = 'C:/Users/Andres/Desktop/CTAnotado/imagenes/Dr Alvarado/'
+filename = 'Estudio1.nii'
 
 # Dest path
-destpath = 'C:/Users/Andres/Desktop/imexhs/Lung/dicomimage/Torax/dcm2png/nuevos_casos_test/' 
+destpath = 'C:/Users/Andres/Desktop/CovidImages/CT/' 
 
 # Load Image
-img = nib.load(path)
+img = nib.load(path+filename)
 img = img.get_fdata()
 
 # Image format
 imgformat = '.png'
 
+#%% Main (Convert nii images in png)
+
+def main():
+    
+    # Size width,lenght and number of slices 
+    [width,length,numslices]=img.shape
+    
+    # Every slide is converted in png
+    for indslic in range(numslices):
+         
+        # Recuerde que está al revés la numeración
+        img_array = img[:,:,numslices-1-indslic]
+        
+        # Image rotation 
+        im_rot=img_array
+        for _ in range(1):
+            im_rot=np.rot90(im_rot)
+               
+        # Window Width (WW) and Window Lenght (WW) in CT
+        L=-500
+        W=1500    
+    
+        # Image transformed into CT window 
+        im_out=window_img_transf(im_rot,L,W)
+        
+        # Image normalization Graylevel->[0,255]
+        norm_img=cv2.normalize(im_out, None, 
+                               alpha = 0, 
+                               beta = 255, 
+                               norm_type = cv2.NORM_MINMAX, 
+                               dtype = cv2.CV_32F)
+        
+        # Image number
+        im_num=indslic+1
+                
+        # Dest filename
+        destfilename='P'+str(patient_no).zfill(4)+'_Im'+str(im_num).zfill(4)+imgformat
+            
+        # Save image
+        cv2.imwrite(destpath+destfilename, norm_img)
+        #print(destfilename)
+    
 #%%
 
 # Transform HU image to Window Image
@@ -37,7 +82,7 @@ def window_img_transf(image, win_center, win_width):
     
     img_min = win_center - win_width // 2
     img_max = win_center + win_width // 2
-    window_image = image.copy()
+    window_image = image
     window_image[window_image < img_min] = img_min
     window_image[window_image > img_max] = img_max
     
@@ -46,45 +91,11 @@ def window_img_transf(image, win_center, win_width):
     
     # Image gray level [0 255]
     window_image_gl=np.uint16((window_image_c/np.max(window_image_c))*255)
+    
+    # Image in selected WW and WL
     window_image_gl=np.uint8(window_image_gl)
         
     return window_image_gl
-
-#%%
-
-def main():
-    [m,n,numslices]=img.shape
-    
-    # Recuerde que está al revés la numeración
-    
-    for i in range(numslices):
-         
-        img_array = img[:,:,numslices-1-i]
-        
-        im_rot=img_array.copy()
-        
-        for _ in range(1):
-            im_rot=np.rot90(im_rot)
-               
-        #a=numslices-1-i
-        
-        # Window width and Window Lenght in CT
-        L=-500
-        W=1500    
-    
-        # Image transformed into CT window 
-        im_out=window_img_transf(im_rot,L,W)
-        
-        norm_img=cv2.normalize(im_out, None, alpha = 0, beta = 255, norm_type = cv2.NORM_MINMAX, dtype = cv2.CV_32F)
-        
-        imnum=i+1
-        
-        filename='P'+str(patient_no).zfill(4)+'_Im'+str(imnum).zfill(4)+'_mask'+imgformat
-        
-        print(filename)
-        
-        cv2.imwrite(destpath+filename, norm_img)
-    
 
 #%%
 if __name__ == "__main__":
