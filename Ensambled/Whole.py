eeb5f72fb4469461eb7e1e99defa086294c54fb5
asandino87 <asandino@unal.edu.co>
2021-05-13 16:45:27 -0500

    up

--- Ensambled/Whole.py
+++ Ensambled/Whole.py
@@ -12,8 +12,6 @@ import cv2
 import numpy as np
 import os
 
-
-
 import tensorflow as tf
 import tensorflow.keras as keras
 from tensorflow.keras.models import load_model
@@ -41,78 +39,92 @@ model_lungsegmentation=load_model('lng_seg_mdl.h5')
 
 dcmimages=[]
 
-#for i in range(len(listfiles)):
-for i in range(1,60):
+scale = 4
 
+#for i in range(len(listfiles)):
+for i in range(30,31):
+
+    """
+    Step 1 -> Prepare dcm image into Lung windows (WW=-500, WW=1500)
+    """
+    
     dcmfilename = listfiles[i]
     
     # Convert .dcm file to graylevel image in window
-    # Lung window (L=-500, W=1500)
+    # Lung window (WL=-500, WW=1500)
    
-    L=-500
-    W=1500
-        
-    [norm_img, ins_num] = dcm_convert(origpath,dcmfilename,L,W)  
+    WL_WW=[-500,1500]
     
+    # Convert dcm image to image in Lung Window, output:
+    # normalized image, instance number
+    [norm_img, ins_num] = dcm_convert(origpath,dcmfilename,WL_WW[0],WL_WW[1])  
+      
+    # Resize Image
     im_array=cv2.resize(norm_img,(512,512), 
-                    interpolation = cv2.INTER_AREA)
-    
+                        interpolation = cv2.INTER_AREA)    
+    # Append CT slices
     dcmimages.append(im_array)
-    
-    scale = 4
 
-    im1=cv2.cvtColor(im_array,cv2.COLOR_GRAY2RGB)
+    """
+    Step 2 -> Lungs segmentation
+    """
 
-    im_array=cv2.resize(im1,(512//scale,512//scale), 
+    # Prepare input image as input in the model 
+    inputimg=cv2.resize(im_array,(512//scale,512//scale), 
                         interpolation = cv2.INTER_AREA)
       
-    # Image gray level normalization
-    im_array=im_array/255
+    # Input image normalization imnorm = im/max(im)
+    norminputimg=inputimg/np.max(inputimg)
     
     # Adding one dimension to array
-    img_array2 = np.expand_dims(im_array,axis=[0])
-
-    pred_mask = model_lungsegmentation.predict(img_array2)
+    norm_inputimg = np.expand_dims(norminputimg,axis=[0])
+    
+    # Predicted mask
+    pred_mask = model_lungsegmentation.predict(norm_inputimg)
     
     # Image mask as (NxMx1) array
     pred_mask = pred_mask[0,:,:,0]
     
-    pred_mask = cv2.resize(pred_mask,(512,512), 
-                       interpolation = cv2.INTER_AREA)
-
-    croppedlungs=im1[:,:,0]*pred_mask
-
+    # Resize predicted mask
+    pred_mask = np.round(cv2.resize(pred_mask,(512,512), 
+                       interpolation = cv2.INTER_AREA))
     
-    lungs_array=cv2.resize(croppedlungs,(512//4,512//4), 
+    # Lungs segmentation
+    segmentedlungs=im_array
+    segmentedlungs[pred_mask==0]=0
+    
+    """
+    Step 3 -> Segment consolidation and ground-glass opacity segmentation
+    """
+    scale = 4
+    lungs_array=cv2.resize(segmentedlungs,(512//scale,512//scale), 
                         interpolation = cv2.INTER_AREA)
     
-    lungs_array=cv2.cvtColor(lungs_array,cv2.COLOR_GRAY2RGB)
-    
+    # Image normalization imnorm = im/max(im)
     lungs_array=lungs_array/255
 
-
-    classes=4    
+    classes=4 # Class{0:Background, 1:Lungs, 2:ground-glass, 3: Consolidation}
 
     lungs_array_tensor = np.expand_dims(lungs_array,axis=[0])
-    segm_pred = model_multiclass.predict(lungs_array_tensor)
-    segm_mask = segm_pred[0,:,:,0]
     
-    pred_maskmulti=np.round(segm_mask*classes)
+    # Multiclass prediction
+    segm_pred = model_multiclass.predict(lungs_array_tensor)
+    segm_pred = segm_pred[0,:,:,0]
+    
+    pred_maskmulti=np.round(segm_pred*classes) # Classes: 1,2,3,4
     pred_maskmulti=pred_maskmulti-1 #Classes: 0,1,2,3
 
-    # Resize predicted mask
-    # pred_mask = cv2.resize(pred_maskmulti,(512,512), 
-    #                       interpolation = cv2.INTER_AREA)
-    
+    # Predicted mask : color and gray
     col_predmask,gray_predmask = getcolormask(pred_maskmulti)
     
-    pred_maskfinal = cv2.resize(col_predmask,(512,512), 
+    # Resize segmentation mask
+    pred_colmaskfinal = cv2.resize(col_predmask,(512,512), 
                            interpolation = cv2.INTER_AREA)
     
     pred_graymaskfinal = cv2.resize(gray_predmask,(512,512), 
                        interpolation = cv2.INTER_AREA)
     
-
+    # Show resutls
     plt.figure()
     io.imshow(color.label2rgb(pred_graymaskfinal,im1[:,:,0],
                           colors=[(0,0,0),(255,0,0),(0,0,255)],
@@ -154,3 +166,61 @@ def getcolormask(inputmask):
     
     
     return colormask,graymask
+
+#%%
+
+def dcm_convert(dcm_dir,dcmfilename,WL,WW): 
+   
+    img_path = dcm_dir+dcmfilename
+
+    # Dicom image read
+    dcm_img = dicom.dcmread(img_path)   # Read dicom
+    instance_number=dcm_img.InstanceNumber # Dicom instace number
+    
+    # Convert dicom image to pixel array
+    img_array = dcm_img.pixel_array
+    
+    # Tranform matrix to HU
+    hu_img = transform_to_hu(dcm_img,img_array)
+    
+    # Compute an image in a window (Lung Window)
+    window_img = window_img_transf(hu_img,WL,WW)
+    
+    w,l = np.shape(window_img)
+
+    window_img = cv2.resize(window_img,(w,l), 
+                        interpolation = cv2.INTER_AREA)
+    
+    window_img= cv2.cvtColor(window_img,cv2.COLOR_GRAY2RGB)
+
+    return window_img, instance_number
+    
+    #dcm_image.WindowCenter
+    #dcm_image.WindowWidth
+
+# Transform dcm to HU (Hounsfield Units)
+def transform_to_hu(medical_image, image):
+    intercept = medical_image.RescaleIntercept
+    #print('intercept')
+    #print(str(intercept))
+    slope = medical_image.RescaleSlope
+    hu_image = image * slope + intercept
+    return hu_image
+
+# Transform HU image to Window Image
+def window_img_transf(image, win_center, win_width):
+    
+    img_min = win_center - win_width // 2
+    img_max = win_center + win_width // 2
+    window_image = image.copy()
+    window_image[window_image < img_min] = img_min
+    window_image[window_image > img_max] = img_max
+    
+    # Gray level bias correction -> from [0 to maxgraylevel]
+    window_image_c=window_image+np.abs(img_min)
+    
+    # Image gray level [0 255]
+    window_image_gl=np.uint16((window_image_c/np.max(window_image_c))*255)
+    window_image_gl=np.uint8(window_image_gl)
+        
+    return window_image_gl
